d0870e64c7b6f8e9a01de30b3ce608ab0c049784
Gandham Dheeraj <121343901+Technoblade19876@users.noreply.github.com>
2023-05-03 18:43:01 +0530

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -1,17 +1,15 @@
 
 	//Get "player1_name" using localStorage.getItem() function and assign it to player1_name
 	//Get "player2_name" using localStorage.getItem() function and assign it to player2_name
+player1_name = localStorage.getItem("player1_name")
+player2_name = localStorage.getItem("player2_name")
+
 
 	player1_score = 0;
 	player2_score = 0;
 
-localStorage.getItem("player1_name",player1_name)
-localStorage.getItem("player2_name",player2_name)
-
-
-
-document.getElementById("player1_name").innerHTML = player1_name + " Score: ";
-document.getElementById("player2_name").innerHTML = player2_name + " Score: ";
+document.getElementById("player1_name").innerHTML = player1_name + " : ";
+document.getElementById("player2_name").innerHTML = player2_name + " : ";
 
 document.getElementById("player1_score").innerHTML = player1_score ;
 document.getElementById("player2_score").innerHTML = player2_score ;
@@ -80,4 +78,6 @@ function check()
 
     document.getElementById("output").innerHTML = "";
 }
+
+
 
